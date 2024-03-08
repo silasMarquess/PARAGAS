d24758f82d84f661f5d5379f9336145feb34acf9
silas <silasmaques@outlook.com>
2024-03-07 23:03:22 -0300

    add folder caixa

---
+++ gasProject/views/caixa/FrmCaixa.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gasProject.views.caixa
+{
+    public partial class FrmCaixa : Form
+    {
+        public FrmCaixa()
+        {
+            InitializeComponent();
+        }
+    }
+}
